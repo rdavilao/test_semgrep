32c504cbe374e942efb198f1d288aa0610d9e117
rdavilao <rdavilao@outlook.com>
2025-02-11 10:06:11 -0500

    Reseting project

--- system/src/main/java/au/perth_latino_chamber/system/exception/DocumentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package au.perth_latino_chamber.system.exception;
-
-public class DocumentNotFoundException extends Exception {
-
-    public DocumentNotFoundException(String message) {
-        super(message);
-    }
-}
--- website/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  host: "https://lccwa.au",
-  systemApiPath: ":8080/api/perth-latino-chamber/",
-  mailApiPath: ":8081/api/perth-latino-chamber/",
-};
